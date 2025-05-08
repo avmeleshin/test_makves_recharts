2afcbb0ad51dd0450af135aac593aeb4887e3b08
aleklsey.v.meleshin <meleshin@npc-ses.ru>
2025-05-08 11:53:44 +0300

    fix pathw

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+
+const router = createBrowserRouter([{
+    path: '/test_makves_recharts/',
+    element: <App />
+}]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
